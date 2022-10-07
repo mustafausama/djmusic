f00806a36839e1afe4d3171e08b2b68a85e0e438
Mustafa Abdelrahman <mustafa.abdelrahman@bld.ai>
2022-10-07 12:20:37 +0200

    fix: Fixed models and migrations

--- djmusic/albums/models.py
+++ djmusic/albums/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 from artists.models import Artist
 
 class Album(models.Model):
   artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
   album_name = models.CharField(max_length=200, default='New Album')
-  created_at = models.DateTimeField(auto_now_add=True)
-  released_at = models.DateTimeField(blank=False, auto_now_add=True)
-  cost = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+  created_at = models.DateTimeField(default=timezone.now)
+  released_at = models.DateTimeField(blank=False)
+  cost = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
   
   def __str__(self):
     return self.album_name
