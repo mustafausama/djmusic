cff54137a5abbc9bde7215be532728f3bb89d827
Mustafa Abdelrahman <mustafa.abdelrahman@bld.ai>
2022-10-13 21:13:12 +0200

    feat: Modified Artist default QuerySet to include approved_albums column

--- djmusic/artists/models.py
+++ djmusic/artists/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from django.db.models import F
+from django.db.models import Count, Q
+
+class ArtistManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().annotate(approved_albums=Count('album__is_approved', filter=Q(album__is_approved=True)))
 
 class Artist(models.Model):
+  objects = ArtistManager()
   stage_name = models.CharField(max_length=200, unique=True, blank=False)
   # TextField has null=False by default
   social_link = models.TextField(blank=True)
