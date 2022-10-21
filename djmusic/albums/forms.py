bd8306521cad15e426f3dffcfb85b9f16c28c227
Mustafa Abdelrahman <mustafa.abdelrahman@bld.ai>
2022-10-21 17:39:46 +0200

    fix: Removed unused classes

--- djmusic/albums/forms.py
+++ djmusic/albums/forms.py
@@ -2,9 +2,6 @@ from django import forms
 
 from .models import Album
 
-class DateTimePickerInput(forms.DateTimeInput):
-    input_type = 'datetime'
-
 class CreateAlbumForm(forms.ModelForm):
   class Meta:
     model = Album
