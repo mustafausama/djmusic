880fe2e0212aaa321423feb6166255cecfb9bd1c
Mustafa Abdelrahman <mustafa.abdelrahman@bld.ai>
2022-10-28 09:52:31 +0200

    feat: Add login and logout functionality

--- djmusic/djmusic/urls.py
+++ djmusic/djmusic/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('artists/', include('artists.urls')),
-    path('albums/', include('albums.urls'))
+    path('albums/', include('albums.urls')),
+    path("accounts/login/", auth_views.LoginView.as_view()),
+    path("accounts/logout/", auth_views.LogoutView.as_view())
 ]
