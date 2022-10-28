fe31c02ac26fed1f950e41b8bc4786410e9420cf
Mustafa Abdelrahman <mustafa.abdelrahman@bld.ai>
2022-10-28 10:12:12 +0200

    feat: Add hyperlinks for login, logout, and creation in artists list

--- djmusic/djmusic/urls.py
+++ djmusic/djmusic/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('artists/', include('artists.urls')),
     path('albums/', include('albums.urls')),
-    path("accounts/login/", auth_views.LoginView.as_view()),
-    path("accounts/logout/", auth_views.LogoutView.as_view())
+    path("accounts/login/", auth_views.LoginView.as_view(), name='login'),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name='logout')
 ]
